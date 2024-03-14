88b40f1a385f5e832cbda33c5baaad1e9c4eabfe
mariyam840 <159921630+mariyam840@users.noreply.github.com>
2024-03-14 13:02:44 +0530

    Update ban.py

--- ban.py
+++ ban.py
@@ -74,10 +74,10 @@ async def start_command(event):
         "Welcome to the Ban All Bot!\n"
         "This bot is designed to ban and unban users in groups",
         buttons=[
-            [Button.url("Support Channel", url="https://t.me/venomOwners")],
-            [Button.url("Support Group", url="https://t.me/venom_chatz")],
+            [Button.url("Support Channel", url="https://t.me/Dil_Ka_Ehsaas")],
+            [Button.url("Support Group", url="https://t.me/ArmanChtaz")],
         ],
-        file='https://te.legra.ph/file/310a7fad596b00513692a.jpg',  # Replace with your image URL
+        file='https://graph.org/file/c2bbf86829c894cfa3b14.jpg',  # Replace with your image URL
     )
 
 
